d951f2c8fc0d3f2ea7ddeb0f447dea696f8f9ce4
bhushan <mbhushan4545@gmail.com>
2024-09-07 00:37:25 +0530

    g

--- #String/ascii.java
+++ #String/ascii.java
@@ -4,7 +4,7 @@ public class ascii {
     public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 System.out.println("enter the charector");
-// char ch=sc.nextLine().charAt(0);
+char ch=sc.nextLine().charAt(0);
 
 
 
