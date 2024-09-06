67f2a801208872e28c9af22f57c8b831b12862e4
bhushan <mbhushan4545@gmail.com>
2024-09-07 00:36:43 +0530

    hhh

--- #String/ascii.java
+++ #String/ascii.java
@@ -4,7 +4,9 @@ public class ascii {
     public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
 System.out.println("enter the charector");
-char ch=sc.nextLine().charAt(0);
+// char ch=sc.nextLine().charAt(0);
+
+
 
 System.out.println(ch+"asci val is "+(int)ch);
     }
---
+++ Pattern.java
@@ -0,0 +1,24 @@
+class Pattern
+{
+    public static void main(String arg[])
+    {
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            // for(int j=i;j<n;j++)
+            // {
+            //     System.out.print(i);
+            // }
+            // for(int j=i;j>=1;j--)
+            // {
+            //     System.out.print(j);
+            // }
+            // System.out.println();
+
+
+           
+
+            
+        }
+    }
+}
